2518724798afd490a2807f1b307fe2b2dd7ea9a9
Izabela <104799350+winternightsz@users.noreply.github.com>
2024-10-11 22:17:41 -0300

    arrumar layout fragmento 3

--- app/src/main/java/com/example/projetoextensao/fragmentos/FragmentoTopico3.java
+++ app/src/main/java/com/example/projetoextensao/fragmentos/FragmentoTopico3.java
@@ -21,7 +21,7 @@ public class FragmentoTopico3 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragmento_topico1, container, false);
+        View view = inflater.inflate(R.layout.fragmento_topico3, container, false);
 
         Spinner filtro = view.findViewById(R.id.spinnerFilter);
 
